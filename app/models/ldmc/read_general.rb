54089cdeb312ea65596563715e2f7cf5ea5d20fd
catalinabustam <catalinabustam@gmail.com>
2014-03-14 11:48:42 -0500

    con views de todo

--- app/models/ldmc/read_general.rb
+++ app/models/ldmc/read_general.rb
@@ -4,12 +4,11 @@ module Ldmc
     include Mongoid::Timestamps
 
     field :type, type: String, default: 'read general'
-
+    field :indication, type: String
     field :read_ann, type: String
-
     field :lesion_dir, type: Array; LESION_OPT = ['periventriculares','sustancia blanca subcortical','sustancia gris','pedunculos cerebrales']
-    field :level_dir
-    field :level_flair
+    field :level_dir, type: String
+    field :level_flair, type: String
 
     embedded_in :subject
 
